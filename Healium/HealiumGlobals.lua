187b1695ceccd8170008c3b18136702f97d3e558
David Arceneaux <david@davidthemachine.org>
2020-10-17 13:12:45 -0500

    figured out how to convert existing backdrop stuffs

---
+++ Healium/HealiumGlobals.lua
@@ -0,0 +1,46 @@
+Healium_Caption = {
+	bgFile = "Interface\\TutorialFrame\\TutorialFrameBackground",
+	edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+	tile = true,
+	edgeSize = 16,
+	tileSize = 32,
+	insets = { left = 5, right = 5, top = 5, bottom = 6 }
+}
+
+Healium_HealButton = {
+	edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+	tile = true,
+	edgeSize = 28,
+	insets = { left = 0, right = 0, top = 0, bottom = 0 }
+}
+
+Healium_Button = {
+	edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+	tile = true,
+	edgeSize = 10,
+	tileSize = 16,
+	insets = { left = 4, right = 4, top = 4, bottom = 4 }
+}
+
+Healium_CurseBar = {
+	edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+	tile = true,
+	edgeSize = 24,
+	insets = { left = 0, right = 0, top = 0, bottom = 0 }
+}
+
+Healium_AggroBar = {
+	edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+	tile = true,
+	edgeSize = 12,
+	insets = { left = 0, right = 0, top = 0, bottom = 0 }
+}
+
+Healium_DropDownList = {
+	bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background-Dark",
+	edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+	tile = true,
+	tileSize = 32,
+	edgeSize = 32,
+	insets = { left = 11, right = 12, top = 12, bottom = 9 }
+}
